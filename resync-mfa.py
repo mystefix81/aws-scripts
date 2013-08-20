7b51fe410b510f1742d6e26fe146a14ecc5bb0b8
Frank Gruellich <frank.gruellich@nokia.com>
2013-08-20 09:10:22 +0200

    Whitespace cleanup.

--- resync-mfa.py
+++ resync-mfa.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('<username>')
 parser.add_argument('<auth_code_1>')
 parser.add_argument('<auth_code_2>')
-args = parser.parse_args()	
+args = parser.parse_args()
 
 user = argv[1]
 auth_code_1 = argv[2]
@@ -17,7 +17,7 @@ conn = boto.connect_iam()
 mfa = conn.get_all_mfa_devices(user)
 serial = mfa["list_mfa_devices_response"]["list_mfa_devices_result"]["mfa_devices"][0]["serial_number"]
 print "your username: " + user
-print "Serial number of your mfa device: " + serial 
+print "Serial number of your mfa device: " + serial
 if conn.resync_mfa_device(user,serial, auth_code_1,auth_code_2):
 	print "Resync MFA device successfull"
 else:
