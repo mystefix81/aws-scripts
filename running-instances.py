244c099404cf254d66f33fef63c70426946c0878
Frank Gruellich <frank.gruellich@nokia.com>
2014-02-28 22:29:52 +0100

    Parallel, faster running-instances.py.

--- running-instances.py
+++ running-instances.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python
-#
-#
-#requires python boto libary and prettytable
-#
-#requires .boto file in your home directory with the following syntax
-#aws_access_key_id= xxxxx
-#aws_secret_access_key= yyyy
-#
-#script will fetch all instances within the AWS account
 
 import boto.ec2
 from boto.ec2.connection import EC2Connection
 from prettytable import PrettyTable
-# EC2Connection() will pick up your key from .boto
-conn = boto.ec2.connection.EC2Connection()
-regions = conn.get_all_regions()
-for region in sorted(regions):
+from threading import Thread
+
+def get_region_instances(region, tables):
     x = PrettyTable(["Name", "Key-Name", "Type", "Placement", "Public-DNS", "Instance-ID", "State", "Launch Time"])
     x.padding_width = 1
     ec2 = boto.ec2.connect_to_region(region.name)
-    instances = ec2.get_all_instances()
-    if instances != []:
-        print region.name
-    for reservation in ec2.get_all_instances():
-        for instance in reservation.instances:
-            x.add_row([instance.tags['Name'],instance.key_name, instance.instance_type, instance.placement, instance.public_dns_name, instance.id, instance.state, instance.launch_time])
-    if instances != []:
-        print x
+    reservations = ec2.get_all_instances()
+    if reservations:
+        for r in reservations:
+            for i in r.instances:
+                x.add_row([
+                    i.tags['Name'],
+                    i.key_name,
+                    i.instance_type,
+                    i.placement,
+                    i.public_dns_name,
+                    i.id,
+                    i.state,
+                    i.launch_time
+                    ])
+        tables[region.name] = x
+    return
+
+conn = boto.ec2.connection.EC2Connection()
+regions = conn.get_all_regions()
+
+threads = {}
+tables = {}
+
+for region in regions:
+    threads[region.name] = Thread(target=get_region_instances, args=[region, tables])
+    threads[region.name].daemon = True
+    threads[region.name].start()
+
+for region in regions:
+    threads[region.name].join()
+
+for region in sorted(tables.keys()):
+    print region
+    print tables[region]
