23557b50c14451fe2d37754b677737a2569cff7c
mystefix81 <jens.c.mueller@here.com>
2014-05-21 11:16:30 +0200

    * added support to use profiles from .boto file

--- running-instances.py
+++ running-instances.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python
 
 import boto.ec2
+import argparse
 from boto.ec2.connection import EC2Connection
 from prettytable import PrettyTable
 from threading import Thread
 
+parser = argparse.ArgumentParser(description="Running ec2 instances in AWS account.")
+parser.add_argument(
+        "-p",
+        "--profile",
+        required=False,
+        help="The profile to use from .boto, if not provided the default credentials will be used."
+        )
+args = parser.parse_args()
+
+if not args.profile:
+    args.profile = ""
+
 def get_region_instances(region, tables):
     x = PrettyTable(["Name", "Key-Name", "Type", "Placement", "Public-DNS", "Instance-ID", "State", "Launch Time"])
     x.padding_width = 1
-    ec2 = boto.ec2.connect_to_region(region.name)
+    ec2 = boto.ec2.connect_to_region(region.name,aws_access_key_id=auth.aws_access_key_id,aws_secret_access_key=auth.aws_secret_access_key)
     reservations = ec2.get_all_instances()
     if reservations:
         for r in reservations:
@@ -33,6 +46,7 @@ def get_region_instances(region, tables):
 
 conn = boto.ec2.connection.EC2Connection()
 regions = conn.get_all_regions()
+auth = boto.connection.AWSAuthConnection("ec2.eu-west-1.amazonaws.com",profile_name=args.profile)
 
 threads = {}
 tables = {}
