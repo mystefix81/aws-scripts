e4b7cb969e1aab5cca4b8dbda295e404c01bbdf3
mystefix81 <jens.mueller@netcheat.de>
2016-02-04 12:40:46 +0100

    restructured some parts of the script

--- running-instances.py
+++ running-instances.py
@@ -1,63 +1,80 @@
 #!/usr/bin/python
+""" running-instances: Getting the runnning instances from a specific account"""
 
 import boto.ec2
 import argparse
-from boto.ec2.connection import EC2Connection
 from prettytable import PrettyTable
 from threading import Thread
 
-parser = argparse.ArgumentParser(description="Show available ec2 instances in AWS account.")
-parser.add_argument(
-        "-p",
-        "--profile",
-        required=False,
-        default=None,
-        help="The profile to use from .boto, if not provided the default credentials will be used."
-        )
-args = parser.parse_args()
-
-def get_region_instances(region, tables):
-    x = PrettyTable(["Name", "Key-Name", "Type", "Placement", "Public-DNS", "Private-IP", "Instance-ID", "State", "Launch Time"])
-    x.padding_width = 1
-    ec2 = boto.ec2.connect_to_region(region.name,aws_access_key_id=auth.aws_access_key_id,aws_secret_access_key=auth.aws_secret_access_key)
+def get_region_instances(auth, region, tables):
+    """Get running instances"""
+    table = PrettyTable(["Name", "Key-Name", "Type", "Placement",
+                     "Public-DNS", "Public-IP", "Private-IP",
+                     "Instance-ID", "State", "Launch Time"])
+    table.padding_width = 1
+    ec2 = boto.ec2.connect_to_region(region.name,
+                                     aws_access_key_id=auth.aws_access_key_id,
+                                     aws_secret_access_key=
+                                     auth.aws_secret_access_key)
     reservations = ec2.get_all_instances()
     if reservations:
-        for r in reservations:
-            for i in r.instances:
+        for reservation in reservations:
+            for i in reservation.instances:
                 try:
                     instance_name = i.tags['Name']
-                except KeyError as e:
+                except KeyError:
                     instance_name = "N/A"
-                    pass
-                x.add_row([
+                table.add_row([
                     instance_name,
                     i.key_name,
                     i.instance_type,
                     i.placement,
                     i.public_dns_name,
+                    i.ip_address,
                     i.private_ip_address,
                     i.id,
                     i.state,
                     i.launch_time
                     ])
-        tables[region.name] = x
+        tables[region.name] = table
     return
 
-conn = boto.ec2.connection.EC2Connection()
-regions = conn.get_all_regions()
-auth = boto.connection.AWSAuthConnection("ec2.eu-west-1.amazonaws.com",profile_name=args.profile)
+def main():
+    """Main function"""
 
-threads = {}
-tables = {}
+    parser = argparse.ArgumentParser(description="Show available ec2"
+                                                 "instances in AWS account.")
+    parser.add_argument(
+            "-p",
+            "--profile",
+            required=False,
+            default=None,
+            help="The profile to use from .boto / .aws/credentials,"
+                 "if not provided the default credentials will be used."
+            )
+    args = parser.parse_args()
+    conn = boto.ec2.connection.EC2Connection()
+    auth = boto.connection.AWSAuthConnection("ec2.eu-west-1.amazonaws.com",
+                                         profile_name=args.profile)
+    regions = conn.get_all_regions()
 
-for region in regions:
-    threads[region.name] = Thread(target=get_region_instances, args=[region, tables])
-    threads[region.name].daemon = True
-    threads[region.name].start()
 
-for region in regions:
-    threads[region.name].join()
+    threads = {}
+    tables = {}
 
-for region in sorted(tables.keys()):
-    print region
-    print tables[region]
+    for region in regions:
+        threads[region.name] = Thread(target=get_region_instances,
+                                      args=[auth, region, tables])
+        threads[region.name].daemon = True
+        threads[region.name].start()
+
+    for region in regions:
+        threads[region.name].join()
+
+    for region in sorted(tables.keys()):
+        print region
+        print tables[region]
+
+if __name__ == '__main__':
+    main()
+
